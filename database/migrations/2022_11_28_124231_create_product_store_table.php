6a60aa7c23a1275001c7d4e8037da61a4a05e8e7
erga916 <erga916@vidalibarraquer.net>
2022-11-28 13:52:11 +0100

    MIGRACIONES

---
+++ database/migrations/2022_11_28_124231_create_product_store_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductStoreTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_store', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('store_id')->constrained();
+            $table->integer('stock');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_store');
+    }
+}

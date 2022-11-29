e2931558aa356a23a7872727225d4bd46a9c040b
alannmrtnz <alanmartiola@gmail.com>
2022-11-29 09:41:00 +0100

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,11 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
             $table->string('username');
             $table->string('address');
             $table->double('accountBalance')->default(1500);
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 

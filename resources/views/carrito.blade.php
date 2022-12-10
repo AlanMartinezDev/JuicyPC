c99aac14d6c705efa8a0d533a67aa8998da3bd58
alannmrtnz <alanmartiola@gmail.com>
2022-12-10 13:50:32 +0100

    carrito acabado y vista show de productos

--- resources/views/carrito.blade.php
+++ resources/views/carrito.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="row">
         <div class="row mb-3">
-            <h1>Productos</h1>
+            <h1>Carrito</h1>
             <!-- If que muestra el número de veces que hemos añadido un producto sumando el 1 de la función add de
                     ShoppingCartController.php-->
             @if(count(Cart::getContent()))
@@ -20,22 +20,20 @@
                     <tr>
                         <td>{{ $item->id }}</td>
                         <td>{{ $item->name }}</td>
-                        <td>{{ $item->price }}</td>
+                        <td>{{ $item->price }}€</td>
                         <td>{{ $item->quantity }}</td>
                         <td>
                             <form action="{{ route('carrito.removeitem') }}" method="post">
                                 @csrf
                                 <input type="hidden" name="id" value="{{ $item->id }}">
-                                <input type="submit" value="Eliminar">
+                                <input type="submit" class="btn btn-outline-danger btn-sm" value="Eliminar">
                             </form>
                         </td>
-                        <td><?php $subtotal = $item->price * $item->quantity; echo $subtotal;?></td>
+                        <td>{{ $item->price * $item->quantity }}€</td>
                     </tr>
                     @endforeach
-                    <tr>
-                        <td>Total: <?php //echo $total;?></td>
-                    </tr>
                 </table>
+                <div class="row">Total: {{ \Cart::getSubTotal() }}€</div>
             @else
                 <a href="/productos">Carrito vacío. Seguir comprando.</a>
             @endif

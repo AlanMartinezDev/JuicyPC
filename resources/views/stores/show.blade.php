8b5e5bd2219436090803779e16b0e588b8afd1b4
erga916 <erga916@vidalibarraquer.net>
2022-12-12 13:39:57 +0100

    AJUSTES VARIOS

--- resources/views/stores/show.blade.php
+++ resources/views/stores/show.blade.php
@@ -1,14 +1,18 @@
 @extends('plantilla')
 @section('content')
+<h5 class="card-title d-flex justify-content-center">{{ $store->name }}</h5><br>
     @foreach($store->products as $stores)
-    <li>
-        Nombre: {{ $stores->name }}
-    </li>
-    <li>
-        Dirección: {{ $stores->address }}
-    </li>
-    <li>
-        Contacto: {{ $stores->contact }}
-    </li>
+    <br><br>
+<div class="row">
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <p class="card-text">Nombre: {{ $stores->brand }} {{ $stores->name }}</p>
+                <p class="card-text">Precio: {{ $stores->price }}€</p>
+                <img src="{{ $stores->image }}" class="card-img-top w-50 d-block ms-auto me-auto" alt="...">
+            </div>
+        </div>
+    </div>
+</div>
     @endforeach
 @endsection
